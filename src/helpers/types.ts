241c0b708d15625059b2d224611b7c2f51895d0e
Kamil Justynski <kamil.justynski1998@gmail.com>
2025-02-04 23:04:21 +0100

    added types for game details

--- src/helpers/types.ts
+++ src/helpers/types.ts
@@ -10,3 +10,32 @@ export interface Game {
   short_description: string;
   thumbnail: string;
 }
+
+export interface GameDetailsScheme {
+  description: string;
+  developer: string;
+  freetogame_profile_url: string;
+  game_url: string;
+  genre: string;
+  minimum_system_requirements: GameDetailsSystem;
+  platform: string;
+  publisher: string;
+  release_date: string;
+  screenshots: GameDetailsScreenshots[];
+  status: string;
+  thumbnail: string;
+  title: string;
+}
+
+export interface GameDetailsSystem {
+  graphics: string;
+  memory: string;
+  os: string;
+  processor: string;
+  storage: string;
+}
+
+export interface GameDetailsScreenshots {
+  id: number;
+  image: string;
+}
