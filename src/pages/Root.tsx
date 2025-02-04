14f1136089565c7cc87a80776fcf4b97bc86855c
Kamil Justynski <kamil.justynski1998@gmail.com>
2025-02-04 23:04:37 +0100

    added new bg

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -2,8 +2,8 @@ import { Outlet, NavLink } from "react-router-dom";
 
 const Root = () => {
   return (
-    <>
-      <nav className="fixed top-0 z-50 w-full border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+    <div className="bg-[url('img/background.png')] min-h-screen">
+      <nav className="fixed top-0 z-50 w-full">
         <div className="px-3 py-3 lg:px-5 lg:pl-3">
           <div className="flex items-center justify-between">
             <div className="flex items-center justify-start rtl:justify-end">
@@ -24,17 +24,17 @@ const Root = () => {
 
       <aside
         id="logo-sidebar"
-        className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full border-r  sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+        className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full sm:translate-x-0 "
         aria-label="Sidebar"
       >
-        <div className="h-full px-3 pb-4 overflow-y-auto dark:bg-gray-800">
+        <div className="h-full px-3 pb-4 overflow-y-auto">
           <ul className="space-y-2 font-medium">
             <li>
               <NavLink
                 to="list"
                 className={({ isActive }) =>
                   `flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group ${
-                    isActive ? "bg-gray-700" : ""
+                    isActive ? "bg-gray-800/60" : ""
                   }`
                 }
               >
@@ -48,7 +48,7 @@ const Root = () => {
                   <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
                   <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
                 </svg>
-                <span className="ms-3">Lista gier</span>
+                <span className="ms-3">Game list</span>
               </NavLink>
             </li>
             <li>
@@ -56,7 +56,7 @@ const Root = () => {
                 to="category"
                 className={({ isActive }) =>
                   `flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group ${
-                    isActive ? "bg-gray-700" : ""
+                    isActive ? "bg-gray-800/60" : ""
                   }`
                 }
               >
@@ -69,7 +69,7 @@ const Root = () => {
                 >
                   <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
                 </svg>
-                <span className="flex-1 ms-3 whitespace-nowrap">Kategorie</span>
+                <span className="flex-1 ms-3 whitespace-nowrap">Category</span>
               </NavLink>
             </li>
             <li>
@@ -77,7 +77,7 @@ const Root = () => {
                 to="favourite"
                 className={({ isActive }) =>
                   `flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group ${
-                    isActive ? "bg-gray-700" : ""
+                    isActive ? "bg-gray-800/60" : ""
                   }`
                 }
               >
@@ -90,19 +90,21 @@ const Root = () => {
                 >
                   <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z" />
                 </svg>
-                <span className="flex-1 ms-3 whitespace-nowrap">Ulubione</span>
+                <span className="flex-1 ms-3 whitespace-nowrap">
+                  Favourite games
+                </span>
               </NavLink>
             </li>
           </ul>
         </div>
       </aside>
 
-      <div className="p-4 bg-gray-300 h-svh sm:ml-64">
+      <div className="p-4 h-full sm:ml-64">
         <div className="p-4 dark:border-gray-700 mt-14">
           <Outlet />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 export default Root;
