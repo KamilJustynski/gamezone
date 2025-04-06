6f77ef562498a1a1f1cde859fcda68d3124d8291
Kamil Justynski <kamil.justynski1998@gmail.com>
2025-04-06 15:29:39 +0200

    implementations aside

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router-dom";
 import { Aside } from "../components/Aside/Aside";
+import { LeftAside } from "../components/Aside/LeftAside";
+import { RightAside } from "../components/Aside/RightAside";
 
 const Root = () => {
   return (
     <div className="bg-[#313030] flex  p-5 h-screen justify-between">
-      <Aside />
-      <div className="flex justify-center overflow-y-auto scrollbar-hide px-5">
+      <Aside children={<LeftAside />} />
+      <div className="w-8/10 flex justify-center overflow-y-auto scrollbar-hide px-5">
         <Outlet />
       </div>
-      <Aside />
+      <Aside children={<RightAside />} />
     </div>
   );
 };
